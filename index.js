55359b37abe99be0cbb3dd497102a205bea8888a
Vicky <vikramramekar001@gmail.com>
2023-09-23 01:51:27 +0530

    save data mongodb

--- index.js
+++ index.js
@@ -2,51 +2,44 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const axios = require('axios');
-const fetch = require('node-fetch');
+app.use(express.json())  
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://instagramposts:SkEjLzNGYbyIeVML@cluster0.4i6kgu3.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 
 let port = 4000;
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello from me!!");
-    console.log(`Hello from me `+new Date()+"\n");
-});
-app.get('/proxy', (req, res) => {
-    const fileUrl = req.query.url; // Get the URL from the query parameter
-    const fileName = req.query.fileName; // Extract the filename from the URL
-    console.log(fileUrl)
-    axios({
-        method: 'get',
-        url: fileUrl,
-        responseType: 'stream',
-    })
-        .then((response) => {
-            res.setHeader('Content-disposition', `attachment; filename=${fileName}`);
-            res.setHeader('Content-type', response.headers['content-type']);
-            res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
-
-            response.data.pipe(res);
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send('Server error');
-        });
+    console.log(`Hello from me ` + new Date() + "\n");
 });
 
-app.get('/graphql/query', (req, res) => {
-    let proxyUrl = 'https://www.instagram.com';
-    fetch(proxyUrl + req.originalUrl, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            "Referer": proxyUrl,
-            "Origin": proxyUrl
-        }
-    })
-        .then(response => response.json())
-        .then(json => res.json(json))
-        .catch(error => res.status(500).json({ error: error.message }));
+app.post('/', async (req, res) => {
+    try {
+        await client.connect();
+        const myDB = client.db("instagram_posts");
+        const myColl = myDB.collection("posts");
+        let doc = req.body;
+        doc.createdAt = new Date();
+        const result = await myColl.insertOne(doc);
+        res.json({ requestBody: result })
+    } catch (ex) {
+        console.log("Failed", ex);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 });
 
 app.listen(port, () => console.log(`Proxy server listening on port ${port}!`));
