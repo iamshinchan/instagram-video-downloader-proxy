4a53c76856847c75421bfaa5758770a711e52052
Vicky <vikramramekar001@gmail.com>
2023-09-23 13:41:29 +0530

    updateall,getall

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const app = express();
 app.use(express.json())  
 
+const dbName = "instagram_posts";
+const postsCollection = "posts";
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://instagramposts:SkEjLzNGYbyIeVML@cluster0.4i6kgu3.mongodb.net/?retryWrites=true&w=majority";
 
@@ -28,8 +31,8 @@ app.get('/', (req, res) => {
 app.post('/', async (req, res) => {
     try {
         await client.connect();
-        const myDB = client.db("instagram_posts");
-        const myColl = myDB.collection("posts");
+        const myDB = client.db(dbName);
+        const myColl = myDB.collection(postsCollection);
         let doc = req.body;
         doc.createdAt = new Date();
         const result = await myColl.insertOne(doc);
@@ -42,6 +45,50 @@ app.post('/', async (req, res) => {
     }
 });
 
-app.listen(port, () => console.log(`Proxy server listening on port ${port}!`));
+app.get('/posts/all', async (req, res) => {
+    try {
+        await client.connect();
+        let arr =[];
+        const myDB = client.db(dbName);
+        const myColl = myDB.collection(postsCollection);
+        const cursor = myColl.find({});
+
+        // Print returned documents
+        for await (const post of cursor) {
+            arr.push(post);
+        }
+        res.json(arr)
+    } catch (ex) {
+        console.log("Failed", ex);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+});
+app.put('/posts', async (req, res) => {
+    try {
+        await client.connect();
+        // Get the "movies" collection in the "sample_mflix" database
+        const database = client.db(dbName);
+        const posts = database.collection(postsCollection);
+        // Create a filter to update all movies with a 'G' rating
+        const filter = { };
+        // Create an update document specifying the change to make
+        let doc = req.body;
+        const updateDoc = {
+            $set: doc,
+        };
+        // Update the documents that match the specified filter
+        const result = await posts.updateMany(filter, updateDoc);
+        res.json(result)
+    } catch (ex) {
+        console.log("Failed", ex);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
 
 module.exports = { app }
