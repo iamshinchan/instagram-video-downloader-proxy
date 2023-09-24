be1e5955c267b583705790a28c9b47dadbe6b487
Vicky <vikramramekar001@gmail.com>
2023-09-24 14:25:21 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
-app.use(express.json())  
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+let port = 4000;
+app.use(cors());
+
 
 const dbName = "instagram_posts";
 const postsCollection = "posts";
@@ -20,8 +26,6 @@ const client = new MongoClient(uri, {
 });
 
 
-let port = 4000;
-app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello from me!!");
@@ -30,28 +34,90 @@ app.get('/', (req, res) => {
 
 app.post('/', async (req, res) => {
     try {
-        await client.connect();
+        await clientConnect();
         const myDB = client.db(dbName);
         const myColl = myDB.collection(postsCollection);
         let doc = req.body;
         doc.createdAt = new Date();
+
+        const response = await fetch(doc.thumbnail_src);
+        const blob = await response.blob();
+        doc.thumbnail_src = 'data:image/png;base64,' + Buffer.from(await blob.arrayBuffer()).toString('base64');
+
         const result = await myColl.insertOne(doc);
         res.json({ requestBody: result })
     } catch (ex) {
         console.log("Failed", ex);
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        await clientClose();
+    }
+});
+
+app.post('/posts/many', async (req, res) => {
+    try {
+        const myDB = client.db(dbName);
+        const myColl = myDB.collection(postsCollection);
+        let arr = req.body.filter(b => b);
+        let promises = [];
+        for (let i = 0; i < arr.length; i++) {
+            arr[i].createdAt = new Date();
+            promises.push(
+                new Promise(async (resolve, reject) => {
+                    try {
+                        const response = await fetch(arr[i].thumbnail_src);
+                        const blob = await response.blob();
+                        arr[i].thumbnail_src = 'data:image/png;base64,' + Buffer.from(await blob.arrayBuffer()).toString('base64');
+                        resolve(arr[i]);
+                    } catch (ex) {
+                        resolve(arr[i]);
+
+                    }
+                })
+            )
+        }
+        await clientConnect();
+        if (promises.length > 0) {
+            Promise.all(promises).then(async (res) => {
+                const result = await myColl.insertMany(arr);
+                res.json({ requestBody: result })
+            })
+        } else {
+            res.json({ requestBody: null });
+        }
+    } catch (ex) {
+        console.log("Failed", ex);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await clientClose();
+    }
+});
+
+app.delete('/:id', async (req, res) => {
+    try {
+        await clientConnect();
+        const myDB = client.db(dbName);
+        const myColl = myDB.collection(postsCollection);
+        const query = { _id: new ObjectId(req.params.id) };
+        const result = await myColl.deleteOne(query);
+        res.json({ requestBody: result })
+    } catch (ex) {
+        console.log("Failed", ex);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await clientClose();
     }
 });
 
 app.get('/posts/all', async (req, res) => {
     try {
-        await client.connect();
-        let arr =[];
+        await clientConnect();
+        let arr = [];
         const myDB = client.db(dbName);
         const myColl = myDB.collection(postsCollection);
-        const cursor = myColl.find({});
+        let params = req.query;
+        console.log(req)
+        const cursor = myColl.find({}).sort({ createdAt: -1 }).skip(parseInt(params.offset)).limit(parseInt(params.limit));
 
         // Print returned documents
         for await (const post of cursor) {
@@ -62,17 +128,17 @@ app.get('/posts/all', async (req, res) => {
         console.log("Failed", ex);
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        await clientClose();
     }
 });
 app.put('/posts', async (req, res) => {
     try {
-        await client.connect();
+        await clientConnect();
         // Get the "movies" collection in the "sample_mflix" database
         const database = client.db(dbName);
         const posts = database.collection(postsCollection);
         // Create a filter to update all movies with a 'G' rating
-        const filter = { };
+        const filter = {};
         // Create an update document specifying the change to make
         let doc = req.body;
         const updateDoc = {
@@ -85,10 +151,25 @@ app.put('/posts', async (req, res) => {
         console.log("Failed", ex);
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        await clientClose();
     }
 });
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+async function clientConnect() {
+    return new Promise(async (resolve, reject) => {
+        await client.connect();
+        resolve(true);
+    })
+}
 
+function clientClose() {
+    return new Promise(async (resolve, reject) => {
+        setTimeout(async () => {
+            await client.close();
+            resolve(true);
+        }, 1500)
+    })
+}
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
 module.exports = { app }
